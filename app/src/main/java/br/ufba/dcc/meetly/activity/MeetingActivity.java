c2e1910b88c3d6cddc5a6adef57c2b1e46866008
Moiteiro <bruno.moiteiro@gmail.com>
2017-03-28 02:35:12 -0300

    Primeiro Rascunho do DatePicker e TimePicker no formulario de Meetings.

--- app/src/main/java/br/ufba/dcc/meetly/activity/MeetingActivity.java
+++ app/src/main/java/br/ufba/dcc/meetly/activity/MeetingActivity.java
@@ -1,12 +1,22 @@
 package br.ufba.dcc.meetly.activity;
 
+import android.app.Dialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import br.ufba.dcc.meetly.R;
 import br.ufba.dcc.meetly.dao.MeetingDAO;
@@ -25,6 +35,16 @@ public class MeetingActivity extends AppCompatActivity {
     private MeetingDAO meetingDAO;
 
 
+    // Datepicker
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
+    // TimePicker
+    public static TimePicker timePicker;
+    private static TextView timeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +54,14 @@ public class MeetingActivity extends AppCompatActivity {
         rootView = findViewById(R.id.activity_meeting);
         meetingFormHelper = new MeetingFormHelper(rootView);
         meetingDAO = new MeetingDAO(this);
+
+        initDatePicker();
+
+        // init TimePicker
+        timeView = (TextView) findViewById(R.id.meeting_time);
+        // end TimePicker
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,5 +89,82 @@ public class MeetingActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    // DatePicker
+    private void initDatePicker()
+    {
+        dateView = (TextView) findViewById(R.id.meeting_date);
+        calendar = Calendar.getInstance();
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view)
+    {
+        showDialog(999);
+    }
+
+    @Override
+    protected DatePickerDialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder()
+                .append(String.format("%02d",  day)).append("/")
+                .append(String.format("%02d",  month)).append("/")
+                .append(year));
+    }
+
+    // TimePicker
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            timeView.setText(new StringBuilder()
+                    .append(String.format("%02d",  hourOfDay)).append(":")
+                    .append(String.format("%02d",  minute)));
+
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    // end Time Picker
 
 }
