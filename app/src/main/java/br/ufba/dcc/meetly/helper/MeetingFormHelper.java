6a7b792f89e1a30f714d5d8a7c3906de575440ac
Lucas Marotta <lucas.casul@gmail.com>
2017-03-29 00:17:29 -0300

    Busca de endereço por CEP

--- app/src/main/java/br/ufba/dcc/meetly/helper/MeetingFormHelper.java
+++ app/src/main/java/br/ufba/dcc/meetly/helper/MeetingFormHelper.java
@@ -7,12 +7,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.apache.commons.validator.GenericValidator;
-import org.w3c.dom.Text;
-
 import br.ufba.dcc.meetly.R;
 import br.ufba.dcc.meetly.models.MeetingModel;
-import br.ufba.dcc.meetly.models.UserModel;
 
 /**
  * Created by StormTrooper on 3/27/2017.
@@ -20,33 +16,46 @@ import br.ufba.dcc.meetly.models.UserModel;
 
 public class MeetingFormHelper
 {
-    private View view;
+    private View formView;
     private Context context;
     private EditText title;
     private EditText subject;
     private TextView date;
     private TextView time;
+    private EditText cep;
     private Spinner state;
 
     public MeetingFormHelper(View view)
     {
-        this.view = view;
+        this.formView = view;
         context = view.getContext();
         init();
     }
 
     private void init()
     {
-        title = (EditText) view.findViewById(R.id.meeting_title);
-        subject = (EditText) view.findViewById(R.id.meeting_subject);
-        date = (TextView) view.findViewById(R.id.meeting_date);
-        time = (TextView) view.findViewById(R.id.meeting_time);
-        state = (Spinner) view.findViewById(R.id.meeting_state);
+        title = (EditText) formView.findViewById(R.id.meeting_title);
+        subject = (EditText) formView.findViewById(R.id.meeting_subject);
+        date = (TextView) formView.findViewById(R.id.meeting_date);
+        time = (TextView) formView.findViewById(R.id.meeting_time);
+        state = (Spinner) formView.findViewById(R.id.meeting_state);
 
-        state = (Spinner) view.findViewById(R.id.meeting_state);
+        state = (Spinner) formView.findViewById(R.id.meeting_state);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.form_states, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         state.setAdapter(adapter);
+
+        cep = (EditText) formView.findViewById(R.id.meeting_cep);
+        cep.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus) {
+                    CepHelper.fillAddressByCep(formView, cep.getText().toString());
+                }
+            }
+        });
+
 
     }
 
